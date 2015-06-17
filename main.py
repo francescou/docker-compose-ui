919322a4d9143d3cdf1d2201e7bc321201c1908e
Francesco Uliana <francesco@uliana.it>
2015-06-17 22:17:46 +0200

    pylint

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+"""
+Docker Compose UI, flask based application
+"""
+
 from flask import Flask, jsonify, request
-from scripts.bridge import ps, get_project
+from scripts.bridge import ps_, get_project
 from scripts.find_yml import find_yml_files
 from json import loads
 import logging
-import os
 
 # Flask Application
 logging.basicConfig(level=logging.DEBUG)
@@ -13,70 +16,78 @@ app = Flask(__name__, static_url_path='')
 projects = find_yml_files('/opt/definitions')
 logging.debug(projects)
 
+def get_project_with_name(name):
+    """
+    get docker compose project given a project name
+    """
+    path = projects[name]
+    return get_project(path)
+
 # REST endpoints
+
 @app.route("/api/containers", methods=['GET'])
 def containers():
-      return jsonify(compose=projects)
+    """
+    List docker compose projects
+    """
+    return jsonify(compose=projects)
 
 @app.route("/api/containers/<name>", methods=['GET'])
 def container(name):
-    path = projects[name]
-    logging.debug('ps ' + path)
-    project = get_project(path)
-    container = ps(project)
-    return jsonify(info=container)
+    """
+    get project details
+    """
+    project = get_project_with_name(name)
+    the_container = ps_(project)
+    return jsonify(info=the_container)
 
 @app.route("/api/logs/<name>", defaults={'limit': "all"}, methods=['GET'])
 @app.route("/api/logs/<name>/<int:limit>", methods=['GET'])
 def logs(name, limit):
-    path = projects[name]
-    logging.debug('logs ' + path)
-    project = get_project(path)
+    """
+    docker-compose logs
+    """
+    lines = {}
+    for k in get_project_with_name(name).containers(stopped=True):
+        lines[k.name] = k.logs(timestamps=True, tail=limit).split('\n')
 
-    logs = {}
-
-    for k in project.containers(stopped=True):
-        logging.debug(k.name)
-        logging.debug(limit)
-        logs[k.name] = k.logs(timestamps=True, tail=limit).split('\n')
-
-    return jsonify(logs=logs)
+    return jsonify(logs=lines)
 
 @app.route("/api/containers/<name>", methods=['DELETE'])
 def kill(name):
-    logging.debug('kill ' + name)
-    path = projects[name]
-    project = get_project(path)
-    outcome = project.kill()
+    """
+    docker-compose kill
+    """
+    outcome = get_project_with_name(name).kill()
     return jsonify(info=outcome)
 
 @app.route("/api/containers", methods=['PUT'])
-def update():
-    j = loads(request.data)
-    name = j["id"]
-    logging.debug('pull ' + name)
-    path = projects[name]
-    project = get_project(path)
-    outcome = project.pull()
+def pull():
+    """
+    docker-compose pull
+    """
+    name = loads(request.data)["id"]
+    outcome = get_project_with_name(name).pull()
     return jsonify(info=outcome)
 
 @app.route("/api/containers", methods=['POST'])
-def up():
-    j = loads(request.data)
-    name = j["id"]
-    logging.debug('up ' + name)
-    path = projects[name]
-    project = get_project(path)
-    logging.debug(project)
-    outcome = project.up()
+def up_():
+    """
+    docker-compose up
+    """
+    name = loads(request.data)["id"]
+    outcome = get_project_with_name(name).up()
     logging.debug(outcome)
     return jsonify(info=len(outcome))
 
 # static resources
 @app.route("/")
 def index():
-  return app.send_static_file('app/index.html')
+    """
+    index.html
+    """
+    return app.send_static_file('app/index.html')
 
 # run app
 if __name__ == "__main__":
-    app.run(host= '0.0.0.0', debug=True, threaded=True)
+    app.run(host='0.0.0.0', debug=True, threaded=True)
--- scripts/find_yml.py
+++ scripts/find_yml.py
@@ -1,10 +1,17 @@
+"""
+find docker-compose.yml files
+"""
+
 import fnmatch
 import os
 
 def find_yml_files(path):
-  matches = {}
-  for root, dirnames, filenames in os.walk(path):
-    for filename in fnmatch.filter(filenames, '*.yml'):
-      key = root[len(path) + 1:]
-      matches[key] = os.path.join(os.getcwd(), root)
-  return matches
+    """
+    find docker-compose.yml files in path
+    """
+    matches = {}
+    for root, _, filenames in os.walk(path):
+        for _ in fnmatch.filter(filenames, '*.yml'):
+            key = root[len(path) + 1:]
+            matches[key] = os.path.join(os.getcwd(), root)
+    return matches
