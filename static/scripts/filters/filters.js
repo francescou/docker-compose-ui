4f8a168e7596331c7fc8b4648d4a389a624452f1
Francesco Uliana <francesco.uliana@cnr.it>
2015-07-02 14:59:59 +0200

    filter by project name

---
+++ static/scripts/filters/filters.js
@@ -0,0 +1,33 @@
+'use strict';
+
+angular.module('staticApp')
+  .filter('filterByName', function () {
+    return function (projects, query) {
+
+      var filteredProjects;
+
+      if (query) {
+
+        filteredProjects = _.chain(projects)
+          .map(function (path, name) {
+            return {
+              name: name,
+              path: path
+            };
+          })
+          .filter(function (item) {
+            return item.name.indexOf(query) >= 0;
+          })
+          .map(function (item) {
+            return [item.name, item.path];
+          })
+          .object()
+          .value();
+
+      } else {
+        filteredProjects = projects;
+      }
+
+      return filteredProjects;
+    };
+  });
