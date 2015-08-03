2f1823b899c785ccc0dd21853c0a8b2771ca9cc9
Francesco Uliana <francesco@uliana.it>
2015-08-03 14:14:08 +0200

    container volumes

--- scripts/bridge.py
+++ scripts/bridge.py
@@ -3,6 +3,8 @@ bridge to docker-compose
 """
 
 from compose.cli.main import TopLevelCommand
+from compose.container import Container
+
 import logging
 
 def ps_(project):
@@ -11,14 +13,23 @@ def ps_(project):
     """
     logging.debug('ps ' + project.name)
     containers = project.containers(stopped=True) + project.containers(one_off=True)
+
     items = map(lambda container: {
-        'name': container.name,
+        'name': container.name_without_project,
         'command': container.human_readable_command,
         'state': container.human_readable_state,
         'ports': container.ports,
+        'volumes': get_volumes(Container.from_id(project.client, container.id)),
         'is_running': container.is_running}, containers)
 
     return items
+
+def get_volumes(container):
+    volumes = container.get('Volumes')
+    volumes_rw = container.get('VolumesRW')
+    return dict(map(lambda (dest, source):
+        [dest, dict(write=volumes_rw[dest],source=source)],
+        volumes.iteritems()))
 
 def get_project(path):
     """
