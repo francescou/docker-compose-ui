0eec9f8eef165f4eee05eec729fe715feef3176d
Francesco Uliana <francesco.uliana@cnr.it>
2017-01-18 11:12:40 +0100

    demo project es6

--- demo-projects/node-redis/app/app.js
+++ demo-projects/node-redis/app/app.js
@@ -1,24 +1,25 @@
-var express = require('express');
-var app = express();
-var redis = require("redis"),
-  client = redis.createClient(6379, 'redis');
+const express = require('express');
+const redis = require("redis");
 
-app.get('/', function(req, res) {
-  var ip = req.connection.remoteAddress;
-  console.log('access from IP ' + ip);
+const app = express();
+const client = redis.createClient(6379, 'redis');
+
+app.get('/', (req, res) => {
+  const ip = req.connection.remoteAddress;
+  console.log(`access from IP ${ip}`);
   client.multi().rpush('ip', ip);
-  res.send('hello ' + ip);
+  res.send(`hello ${ip}`);
 });
 
-app.get('/health', function(req, res) {
-  var redisVersion = client.server_info.redis_version;
-  if (redisVersion === '3.2.6') {
+app.get('/health', (req, res) => {
+  const redisVersion = client.server_info.redis_version;
+  if (redisVersion.startsWith('3')) {
     res.send({status: 'UP'});
   } else {
-    res.status(500).send('v. ' + redisVersion);
+    res.status(500).send(`version ${redisVersion}`);
   }
 });
 
-app.listen(8080, function () {
+app.listen(8080, () => {
   console.log('Example app listening on port 8080!');
 });
