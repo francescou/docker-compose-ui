a69758a5ed20407b0850fcdff8daffcb4c65e94c
Francesco Uliana <francesco.uliana@cnr.it>
2017-01-18 10:56:42 +0100

    demo project node redis using expressjs

--- demo-projects/node-redis/app/app.js
+++ demo-projects/node-redis/app/app.js
@@ -1,15 +1,24 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 var redis = require("redis"),
   client = redis.createClient(6379, 'redis');
 
-client.on("error", function (err) {
-  console.log("Error " + err);
-});
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+app.get('/', function(req, res) {
   var ip = req.connection.remoteAddress;
   console.log('access from IP ' + ip);
   client.multi().rpush('ip', ip);
-  res.end('Hello ' + ip);
-}).listen(8080, '0.0.0.0');
+  res.send('hello ' + ip);
+});
+
+app.get('/health', function(req, res) {
+  var redisVersion = client.server_info.redis_version;
+  if (redisVersion === '3.2.6') {
+    res.send({status: 'UP'});
+  } else {
+    res.status(500).send('v. ' + redisVersion);
+  }
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
