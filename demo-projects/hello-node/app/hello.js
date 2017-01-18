0d66a1066a80ca561959a46465d5764ce52f8843
Francesco Uliana <francesco.uliana@cnr.it>
2017-01-18 11:09:23 +0100

    demo project es6

--- demo-projects/hello-node/app/hello.js
+++ demo-projects/hello-node/app/hello.js
@@ -1,5 +1,6 @@
-var http = require('http');
-http.createServer(function (req, res) {
+const http = require('http');
+
+http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
 }).listen(1337, '0.0.0.0');
