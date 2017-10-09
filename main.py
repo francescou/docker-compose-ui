7547ebc5b75db39c7b38bc01e73cf26986ae720c
Thomas LEVEIL <thomasleveil@gmail.com>
2017-10-09 21:31:06 +0200

    clean my mess

--- main.py
+++ main.py
@@ -489,4 +489,4 @@ def handle_generic_error(err):
 
 # run app
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, threaded=False)
+    app.run(host='0.0.0.0', debug=False, threaded=True)
